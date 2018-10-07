33ecbd38abb0c60948b6ad8f10229a77472cccc4
yasshy2 <yasshy2@gmail.com>
2018-10-07 18:28:42 +0900

    デバッグ用にログイン時のソース表示

--- src/test/java/net/rushhourgame/it/CommonAction.java
+++ src/test/java/net/rushhourgame/it/CommonAction.java
@@ -75,6 +75,9 @@ public class CommonAction {
         // サインインボタンの押下
         signin.sendKeys(Keys.ENTER);
 
+        LOG.log(Level.INFO, "URL : " + driver.getCurrentUrl());
+        LOG.log(Level.INFO, driver.getPageSource());
+
         // 名前とパスワードを入力
         WebElement name = driver.findElement(By.id("username_or_email"));
         name.sendKeys(TWITTER_USER_NAME);
